6c6f04354035b1d7a3a0922d51cd715795db5482
Gunnsteinn Þórisson <gussi@gussi.is>
2013-08-21 22:35:24 +0000

    Update log.class.php
    Önnur útfærsla

--- src/log.class.php
+++ src/log.class.php
@@ -2,8 +2,18 @@
 
 class log {
     private $log;
+    public static $instance;
+    
     public function __construct($file){
         $this->log = fopen($file, "a");
+        self::instance = $this;
+    }
+    
+    static public function getInstance() {
+        if (self::instance == null) {
+            return new log();
+        }
+        return self::instance;
     }
 
     public function write($entry){
